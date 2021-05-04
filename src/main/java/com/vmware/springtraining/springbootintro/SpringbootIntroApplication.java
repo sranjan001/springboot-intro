9db1132efa20daba7d13de851dfe65d9536fefd4
sranjan001 <sranjan@pivotal.io>
2021-05-04 19:59:17 +0800

    Api controller

--- src/main/java/com/vmware/springtraining/springbootintro/SpringbootIntroApplication.java
+++ src/main/java/com/vmware/springtraining/springbootintro/SpringbootIntroApplication.java
@@ -2,6 +2,10 @@ package com.vmware.springtraining.springbootintro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringbootIntroApplication {
@@ -10,4 +14,13 @@ public class SpringbootIntroApplication {
         SpringApplication.run(SpringbootIntroApplication.class, args);
     }
 
+    @RestController
+    @RequestMapping("/api")
+    public class ApiController {
+
+        @GetMapping("/greeting")
+        public String getGreeting() {
+            return "Hello world from API";
+        }
+    }
 }
