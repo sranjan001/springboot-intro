3bb5430bbfb43b15d17c28caec51020e41c7084c
sranjan001 <sranjan@pivotal.io>
2021-05-04 19:12:50 +0800

    first commit

---
+++ src/test/java/com/vmware/springtraining/springbootintro/SpringbootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.springtraining.springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
